513c9e4f57dfcdc09193b31d586bb44519e85832
natan-do-egito-roldao <eusuez@gmail.com>
2025-07-11 15:55:39 -0300

    alterando chamado da variavel do mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.Mongo_URI)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('✅ Conexão com o MongoDB estabelecida com sucesso')
     } catch (err) {
         console.log('❌ Erro ao conectar ao MongoDB:', err.message)
